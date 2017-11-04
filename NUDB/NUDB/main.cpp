4e75d0033a4dcee304c72e55412765ce3802627b
Edward <luoshixin1993@gmail.com>
2017-11-03 22:36:21 -0500

    Add files via upload
    Upload project file

---
+++ NUDB/NUDB/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  NUDB
+//
+//  Created by Shixin Luo on 11/3/17.
+//  Copyright © 2017 Shixin Luo. All rights reserved.
+//
+
+#include <mysql.h>
+#include <iostream>
+using namespace std;
+
+int main (int argc, const char* argv[]) {
+    MYSQL *connection, mysql;
+    mysql_init(&mysql);
+    connection = mysql_real_connect(&mysql, "localhost", "root", "3604818", "project3-nudb", 0, 0, 0);
+    if (connection == NULL) {
+        //unable to connect
+        printf("Oh Noes!\n");
+    } else {
+        printf("You are now connected. Welcome!\n");
+    }
+}
